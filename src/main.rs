abd8839530a44623df96673c5ab262cccb9b9484
eddy_hummel <eddy.hummel@gmail.com>
2023-09-17 20:29:48 +0200

    bruh ca fonctionne pas je test autre chose

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,22 @@
-use async_hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Request, Response, Server};
-use std::convert::Infallible;
-use std::net::SocketAddr;
-use tokio::sync::Mutex;
-use tokio::time::{self, Duration};
+use rouille::Request;
 
 mod services::WifiService;
 use services::WifiService::WifiService;
 
-#[tokio::main]
+#[actix_web::main]
 async fn main() {
     let mut wifi = WifiService::new("", ""); //ssid, passwd
-    let addr = SocketAddr::from((wifi.host, wifi.port)); // Adresse IP et port à utiliser
-
-    let make_svc = make_service_fn(|_conn| {
-        let state = State::default();
-        async { Ok::<_, Infallible>(service_fn(move |req| handle_request(req, state.clone()))) }
-    });
-
-    let server = Server::bind(&addr).serve(make_svc);
-    if let Err(e) = server.await {
-        eprintln!("Server error: {}", e);
-    }
+    rouille::start_server("0.0.0.0:8080", move |request| {
+        router(request)
+    });    
+    //let addr = SocketAddr::from((wifi.host, wifi.port)); // old version
 }
 
-#[derive(Default)]
-struct State {
-    led_on: Mutex<bool>,
-}
-
-async fn handle_request(
-    req: Request<Body>,
-    state: State,
-) -> Result<Response<Body>, Infallible> {
-    match (req.method(), req.uri().path()) {
-        (&hyper::Method::GET, "/api/led/toggle") => {
-            let mut led_on = state.led_on.lock().await;
-            *led_on = !*led_on;
-
-            // Simuler le clignotement de la LED en basculant son état toutes les 1 seconde
-            let delay = time::sleep(Duration::from_secs(1));
-            tokio::pin!(delay);
-            while delay.await.is_pending() {
-                if *led_on {
-                    // Allumez la LED ici
-                } else {
-                    // Éteignez la LED ici
-                }
-            }
-
-            // Répondez avec un message indiquant l'état actuel de la LED
-            let response_text = if *led_on { "LED allumée" } else { "LED éteinte" };
-            Ok(Response::new(Body::from(response_text)))
-        }
-        _ => Ok(Response::builder()
-            .status(404)
-            .body(Body::empty())
-            .unwrap()),
-    }
+fn router(request: &Request) -> rouille::Response {
+    router!(request,
+        (GET) (/) => {
+            rouille::Response::text("Hello, World!")
+        },
+        _ => rouille::Response::empty_404()
+    )
 }
