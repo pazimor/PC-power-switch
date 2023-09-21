54569e0b36e9eab2683b6030551d682cf4d21824
eddy_hummel <eddy.hummel@gmail.com>
2023-09-21 19:05:23 +0200

    WIP(rust): preparing for new approch

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,6 @@
-use rouille::Request;
-
 mod services::WifiService;
 use services::WifiService::WifiService;
 
-#[actix_web::main]
 async fn main() {
-    let mut wifi = WifiService::new("", ""); //ssid, passwd
-    rouille::start_server("0.0.0.0:8080", move |request| {
-        router(request)
-    });    
-    //let addr = SocketAddr::from((wifi.host, wifi.port)); // old version
-}
-
-fn router(request: &Request) -> rouille::Response {
-    router!(request,
-        (GET) (/) => {
-            rouille::Response::text("Hello, World!")
-        },
-        _ => rouille::Response::empty_404()
-    )
+    
 }
--- src/services/WifiService.rs
+++ src/services/WifiService.rs
@@ -1,7 +1,3 @@
-use esp8266_hal::prelude::*;
-use esp8266_hal::wifi::station::Station;
-use esp8266_hal::wifi::Mac;
-use nb::block;
 
 pub struct WifiService {
     ssid: str,
@@ -12,37 +8,7 @@ pub struct WifiService {
 }
 
 impl WifiService {
-    pub fn new(ssid: &str, passwd: &str) -> Self {
-        //faire un controller GPIO
-        let dp = pac::Peripherals::take().unwrap();
-        // Configure les broches GPIO nécessaires pour la connexion Wi-Fi
-        let mut gpio = dp.GPIO.split();
-        let _ = gpio.gpio2.into_function::<esp8266_hal::gpio::Input>();
-        let _ = gpio.gpio0.into_function::<esp8266_hal::gpio::Input>();
-        let _ = gpio.gpio15.into_function::<esp8266_hal::gpio::Input>();
-
-        // Initialise le matériel Wi-Fi
-        return WifiController {
-            wifi: Station::new(dp.TMRA, dp.TMRB, dp.WIFI, dp.SYSCON, dp.CLOCK),
-            ssid,
-            passwd,
-            host: [127, 0, 0, 1],
-            port: 8080,
-        }
-    }
-    pub fn is_connected(&self) -> bool {
-        let status = Ok(wifi.get_status());
-        if status == esp8266_hal::wifi::StationStatus::GotIp {
-            return true;
-        }
-        return false;
-    }
-    pub fn setup(&self) {
-        // Configure le réseau Wi-Fi
-        wifi.connect(
-            self.ssid, self.passwd, None,
-            &Mac::broadcast(), None, None,
-        ).unwrap();
-        self.is_connected(self)
-    }
+    pub fn new(ssid: &str, passwd: &str) -> Self { /**/ }
+    pub fn is_connected(&self) -> bool { /*am i connected*/ }
+    pub fn setup(&self) {/*Configure Wi-Fi*/}
 }
